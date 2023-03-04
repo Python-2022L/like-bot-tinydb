5cba5e9092b3dbe0b4dd693bedbd83b09fbec1ab
JavohirJalilov <javohirjaliloff@gmail.com>
2023-03-04 15:13:16 +0500

    finally like dislike database

--- db.py
+++ db.py
@@ -1,31 +1,46 @@
 from tinydb import TinyDB
+from tinydb.database import Document
 
 class LikeDB:
     def __init__(self, file_path):
         # Initialize the database
-        self.db = TinyDB(file_path)
+        self.db = TinyDB(file_path, indent=4)
 
     def add_student(self, user_id):
         """
         Add student in Database
         """
-        pass
-        
-        return self.db[user_id]
+        cond = self.db.contains(doc_id=user_id)
+        if not cond:
+            document = Document({"like":0, "dislike":0}, doc_id=user_id)
+            self.db.insert(document)
+            return {"status": "Added student"}
+        else:
+            return {"status": "Already exist"}
     
     def all_likes(self):
         """
         Count all users likes
         """
-        pass
+        data = self.db.all()
+        count = 0
+
+        for i in data:
+            count += i['like']
+        return count
 
     def all_dislikes(self):
         """
         Count all users dislikes
         """
-        pass
+        data = self.db.all()
+        count = 0
 
-    def add_like(self, user_id:str):
+        for i in data:
+            count += i['dislike']
+        return count
+
+    def add_like(self, user_id:int):
         """
         Add like to the database
         
@@ -34,9 +49,20 @@ class LikeDB:
         Returns:
             The number of likes and dislikes for the post
         """
-        pass
+        data = self.db.get(doc_id=user_id)
 
-    def add_dislike(self, user_id:str):
+        like = data.get("like")
+
+        if like == 1:
+            data["like"] = 0
+        elif like == 0:
+            data["like"] = 1
+            data["dislike"] = 0
+        self.db.update(data, doc_ids=[user_id])
+
+
+
+    def add_dislike(self, user_id:int):
         """
         Add dislike to the database
         
@@ -45,8 +71,17 @@ class LikeDB:
         Returns:
             The number of likes and dislikes for the post
         """
-        pass
-            
+        
+        data = self.db.get(doc_id=user_id)
+
+        dislike = data.get("dislike")
+
+        if dislike == 1:
+            data["dislike"] = 0
+        elif dislike == 0:
+            data["dislike"] = 1
+            data["like"] = 0
+        self.db.update(data, doc_ids=[user_id])
 
 
 
